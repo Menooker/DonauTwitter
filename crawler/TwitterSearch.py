72e2bc822ea71ed7110c5b330a1d3e22d3a6a97f
Menooker <myjisgreat@live.cn>
2018-04-25 22:10:17 +1000

    Try to handle error 429

--- crawler/TwitterSearch.py
+++ crawler/TwitterSearch.py
@@ -31,39 +31,47 @@ def do_search(api,db,keyword_query,geocode,from_id,to_id,next_id):
         to_id=0
     count=0
     pager = TwitterPager(api, 'search/tweets', {'q': keyword_query, 'geocode': geocode,  'count': '100', 'max_id': str(from_id), 'since_id' : str(to_id)})
-    for item in try_get_iterator(pager):
-        #print(item)
-        if 'text' in item:
-            #try:
-            if True:
-                #print item["id"]
-                cur_id=int(item["id"])
-                #if next_id != -1, we run in re-start mode, don't reset next_id
-                #else we need to update next_id when the first item arrives in this iteration
-                #and next iteration's to_id will be set to next_id of this iteration 
-                if next_id==-1:
-                    next_id=cur_id
-                if cur_id<=to_id:
-                    break
-                info=get_dict_object_from_tweet(item)
-                if not info:
-                    print "Error parsing the tweet, ignore it"
-                    continue
-                #print info
-                db.put(info)
-                count+=1
-                if count % 1000 == 0:
-                    print count
-                #print item["id"],"ok"
-                #print(info["post_text"])
-            #except TwitterAPI.TwitterError.TwitterRequestError,e:
-            #    if e.status_code==429
+    while True:
+        try:
+            for item in pager.get_iterator():
+                #print(item)
+                if 'text' in item:
+                    #try:
+                    if True:
+                        #print item["id"]
+                        cur_id=int(item["id"])
+                        #if next_id != -1, we run in re-start mode, don't reset next_id
+                        #else we need to update next_id when the first item arrives in this iteration
+                        #and next iteration's to_id will be set to next_id of this iteration 
+                        if next_id==-1:
+                            next_id=cur_id
+                        if cur_id<=to_id:
+                            break
+                        info=get_dict_object_from_tweet(item)
+                        if not info:
+                            print "Error parsing the tweet, ignore it"
+                            continue
+                        #print info
+                        db.put(info)
+                        count+=1
+                        if count % 1000 == 0:
+                            print count
+                        #print item["id"],"ok"
+                        #print(info["post_text"])
+                    #except TwitterAPI.TwitterError.TwitterRequestError,e:
+                    #    if e.status_code==429
 
-            progress.update(cur_id,to_id,next_id)
-        elif 'message' in item:
-            # something needs to be fixed before re-connecting
-            raise Exception(item['message'])
-    return count
+                    progress.update(cur_id,to_id,next_id)
+                elif 'message' in item:
+                    # something needs to be fixed before re-connecting
+                    raise Exception(item['message'])
+            return count
+        except TwitterAPI.TwitterError.TwitterRequestError,e:
+            if e.status_code==429:
+                print ("Too Many Requests, now sleeping...")
+                sleep(60)
+            else:
+                raise e
 
 def search(api,db,keywords,geocode):
     keyword_query = " OR ".join(keywords)
