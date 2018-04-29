b3db59c67fc640364d2d587a90ee4437883e41a0
Menooker <myjisgreat@live.cn>
2018-04-29 21:25:53 +1000

    add comments|| will not  update if not told

--- analytics/TwitterAnalytics.py
+++ analytics/TwitterAnalytics.py
@@ -1,7 +1,13 @@
 import couchdb_map
 
 def testfunc(tweet):
-    tweet["k"]+=1
-    tweet["v"]=tweet["k"]+10086
+    if tweet["k"]==7:
+        #we want to update the document, return true
+        tweet["k"]+=1
+        tweet["v"]=tweet["k"]+10086
+        return True
+    else:
+        #we don't want to update the document, return false
+        return False
 
 couchdb_map.map(testfunc,"http://localhost:5984","test")
--- analytics/couchdb_map.py
+++ analytics/couchdb_map.py
@@ -1,5 +1,14 @@
 import couchdb
 
+'''
+map all documents in the db of the url with the map function
+Params:
+    func: the function to process the documents, the parameter of the func is a dict of a document. The func must
+        return True if it wants to update the document
+    url: the url of the db
+    dbname: the name the db
+    batch_size: the size of one batch of iteration
+'''
 def map(func,url,dbname,batch_size=5000):
     server = couchdb.Server(url=url)
     db = server[dbname]
@@ -12,7 +21,7 @@ def map(func,url,dbname,batch_size=5000):
     for i in range(batches):
         for row in db.view("_all_docs", skip=i*batch_size,limit=batch_size):
             data = db.get(row.id)
-            func(data)
-            db.save(data)
+            if func(data):
+                db.save(data)
         print "Batch", i ,"done"
         
