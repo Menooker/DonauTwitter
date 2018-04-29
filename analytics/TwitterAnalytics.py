2ade156c6ea6e3a89609896a0651c11efdd00a10
Menooker <myjisgreat@live.cn>
2018-04-29 22:05:07 +1000

    add map_new

--- analytics/TwitterAnalytics.py
+++ analytics/TwitterAnalytics.py
@@ -10,4 +10,16 @@ def testfunc(tweet):
         #we don't want to update the document, return false
         return False
 
-couchdb_map.map(testfunc,"http://localhost:5984","test")
+def testfunc_new(tweet):
+    ret=dict()
+    if tweet["k"]<100:
+        #we want to update the document, return true
+        ret["k"]=tweet['k']+20
+        ret['_id']=tweet['_id']
+        return ret
+    else:
+        #we don't want to update the document, return false
+        return None
+
+#couchdb_map.map(testfunc,"http://localhost:5984","test")
+couchdb_map.map_new(testfunc_new,"http://localhost:5984","test","test_result")
--- analytics/couchdb_map.py
+++ analytics/couchdb_map.py
@@ -6,7 +6,7 @@ Params:
     func: the function to process the documents, the parameter of the func is a dict of a document. The func must
         return True if it wants to update the document
     url: the url of the db
-    dbname: the name the db
+    dbname: the name of the db
     batch_size: the size of one batch of iteration
 '''
 def map(func,url,dbname,batch_size=5000):
@@ -25,3 +25,31 @@ def map(func,url,dbname,batch_size=5000):
                 db.save(data)
         print "Batch", i ,"done"
         
+
+'''
+map all documents in the db of the url with the map function, and write to another db
+Params:
+    func: the function to process the documents, the parameter of the func is a dict of a document. The func should
+     return a dict to insert a document or return None
+    url: the url of the db
+    dbname: the name of the source db
+    outdbname: the output db name
+    batch_size: the size of one batch of iteration
+'''
+def map_new(func,url,dbname,outdbname,batch_size=5000):
+    server = couchdb.Server(url=url)
+    db = server[dbname]
+    outdb= server[outdbname]
+
+    doc_count = db.info()["doc_count"]
+    batches = doc_count/batch_size
+    if doc_count % batch_size!=0 :
+        batches+=1
+
+    for i in range(batches):
+        for row in db.view("_all_docs", skip=i*batch_size,limit=batch_size):
+            data = db.get(row.id)
+            newdata = func(data)
+            if newdata:
+                outdb.save(newdata)
+        print "Batch", i ,"done"
